fff5881b6a7f7d5dcce61ac24b290b31c4775174
Artur Manuilenko <artur.manuilenko@yalantis.net>
2023-09-26 11:30:39 +0300

    feat: add App tests

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -3,4 +3,6 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
+  printWidth: 120,
+  useTabs: false,
 };
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,26 @@
-import { describe, it } from 'vitest';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { describe } from 'vitest';
 
-describe('Test', () => {
-  it('true', () => {
-    expect(true).toBeTruthy();
+import App from './App';
+
+describe('App', () => {
+  beforeEach(() => {
+    render(<App />);
+  });
+
+  test('has H1 with text: Vite + React', () => {
+    const text = screen.getByRole('heading', { level: 1, name: 'Vite + React' });
+
+    expect(text).toBeInTheDocument();
+  });
+
+  test('increments count when button is clicked', () => {
+    const button = screen.getByRole('button', { name: 'count is 0' });
+
+    expect(button).toBeInTheDocument();
+    fireEvent.click(button);
+
+    const updatedButton = screen.getByRole('button', { name: 'count is 1' });
+    expect(updatedButton).toBeInTheDocument();
   });
 });
