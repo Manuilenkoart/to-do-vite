ef7c2966a9d98a4425da2fe8c9f1226d59f6c1ba
Artur Manuilenko <artur.manuilenko@yalantis.net>
2023-11-04 11:53:31 +0200

    refactor: change connect location of global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Provider } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+
+import { GlobalStyles } from '@/styles';
 
 import { router } from './router';
 import { store } from './store';
 
 function App() {
   return (
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <>
+      <ToastContainer />
+      <GlobalStyles />
+
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </>
   );
 }
 
