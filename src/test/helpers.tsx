fcfe7f158c5d2bf0a165eebb3c68ab53011943f2
Artur Manuilenko <artur.manuilenko@yalantis.net>
2023-11-04 21:41:11 +0200

    feat: NoMatch test

--- src/test/helpers.tsx
+++ src/test/helpers.tsx
@@ -1,10 +1,20 @@
 import { render as rtlRender } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { ReactElement } from 'react';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import { store } from '@/store';
 
 export const renderWithStore = (Component: ReactElement) =>
   rtlRender(Component, { wrapper: ({ children }) => <Provider store={store}>{children}</Provider> });
 
+export const renderWithRouter = (Component: ReactElement, { route = '/' } = {}) => {
+  window.history.pushState({}, 'Test page', route);
+
+  return {
+    user: userEvent.setup(),
+    ...rtlRender(Component, { wrapper: BrowserRouter }),
+  };
+};
 export const generateString = (length: number) => 'x'.repeat(length);
