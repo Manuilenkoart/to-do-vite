8fdb91af74c0e717068b165c0814ffe841cfe42d
Artur Manuilenko <artur.manuilenko@yalantis.net>
2024-01-06 12:44:36 +0200

    feat: custom animation 404 page

---
+++ src/router/components/animatedFace/animatedFace.tsx
@@ -0,0 +1,12 @@
+import * as S from './animatedFace.styled';
+
+function AnimatedFace() {
+  return (
+    <S.Face>
+      <S.EyeLeft />
+      <S.EyeRight />
+      <S.Mouth />
+    </S.Face>
+  );
+}
+export default AnimatedFace;
